deaeec0390a9119d5e8137c20b7b021fd8c288d4
Palanath <pala@dusttoash.org>
2022-12-26 02:39:12 -0600

    Remove "fast" mode option which wasn't used

--- src/pala/tools/fenc/Options.java
+++ src/pala/tools/fenc/Options.java
@@ -4,12 +4,11 @@ import pala.libs.generic.parsers.cli.CLIParams;
 
 public class Options {
 	private final String key;
-	private final boolean decryptionMode, fastMode;
+	private final boolean decryptionMode;
 
 	public Options(CLIParams params) {
 		key = params.readString((String) null, "-k", "--key");
 		decryptionMode = params.checkFlag(false, "--dec", "--decrypt", "-d");
-		fastMode = params.checkFlag(false, "--fast", "-f");
 	}
 
 	/**
@@ -32,24 +31,6 @@ public class Options {
 	 */
 	public boolean isDecryptionMode() {
 		return decryptionMode;
-	}
-
-	/**
-	 * Specifies whether <i>fast/unsafe mode</i> is enabled. Normally, the program
-	 * copies each file, (one at a time), to a temporary directory, operates on
-	 * (encrypts/decrypts) it in place, and then copies the file back. This prevents
-	 * the original files from being damaged in case an error occurs while
-	 * encrypting or decrypting. If fast mode is on, the original files are not
-	 * copied to a temporary directory and then operated on; the original files are
-	 * instead operated on in place.
-	 * 
-	 * @flag --fast -f
-	 * @defaultValue <code>false</code>
-	 * @return <code>true</code> if fast mode is enabled, <code>false</code>
-	 *         otherwise.
-	 */
-	public boolean isFastMode() {
-		return fastMode;
 	}
 
 }
