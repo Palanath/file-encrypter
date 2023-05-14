a1f691cf345154c7fdc36dd822ed49bba75d15b6
Palanath <pala@dusttoash.org>
2023-05-14 01:33:10 -0500

    Update EncryptionProcessor to use CipherProcessor

--- src/pala/tools/fenc/processing/EncryptionProcessor.java
+++ src/pala/tools/fenc/processing/EncryptionProcessor.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -18,7 +16,6 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-import pala.libs.generic.util.Hashing;
 import pala.tools.fenc.logging.BranchLogger;
 import pala.tools.fenc.logging.MessageLogger;
 import pala.tools.fenc.logging.PeriodicSuccessLogger;
@@ -30,14 +27,9 @@ import pala.tools.fenc.logging.PeriodicSuccessLogger;
  * @author Palanath
  *
  */
-public class EncryptionProcessor implements DirectoryProcessor {
+public class EncryptionProcessor extends CipherProcessor {
 
 	public static final String HASH_STRING = "Encrypted by FEnc.";
-
-	private final byte[] keyHash, fileHeader;
-	private final int bufferSize;
-
-	private final BranchLogger logger;
 
 	public EncryptionProcessor(String key, int bufferSize, MessageLogger logger) {
 		this(key, bufferSize, new BranchLogger(logger));
@@ -48,62 +40,7 @@ public class EncryptionProcessor implements DirectoryProcessor {
 	}
 
 	private EncryptionProcessor(String key, int bufferSize, BranchLogger logger) {
-		keyHash = Hashing.sha256(key);
-		fileHeader = Hashing.sha256(HASH_STRING + key + HASH_STRING);
-		this.bufferSize = bufferSize;
-		this.logger = logger;
-	}
-
-	@Override
-	public void processFile(File f) {
-
-		try {
-			if (f.length() == 0)
-				return;
-			File temp;
-			try {
-				// Create a temp file as the destination for the encryption/decryption.
-				temp = File.createTempFile("enc", null);
-			} catch (IOException e) {
-				logger.failure("TMPF",
-						"Failed to create the temporary file (for intermediary processing) that file, " + f
-								+ ", would get encrypted then written to. (The file was NOT encrypted.) [Err msg: "
-								+ e.getLocalizedMessage() + ']');
-				return;
-			}
-			temp.deleteOnExit();
-
-			try {
-				encryptFile(temp, f, bufferSize, fileHeader, keyHash);
-			} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException
-					| InvalidAlgorithmParameterException e) {
-				logger.failure("EFL", "Failed to initialize the encryption algorithm while processing file: " + f
-						+ ". [Err msg: " + e.getLocalizedMessage() + ']');
-				return;
-			} catch (IOException e) {
-				logger.failure("IOEX",
-						"Encountered a file in-out exception while trying to read or write and encrypt the file " + f
-								+ ". [Err msg: " + e.getLocalizedMessage() + ']');
-				return;
-			} catch (FileProcessingException e) {
-				logger.failure("ENEX", "Encountered an encryption failure while trying to encrypt the file " + f
-						+ ". [Err msg: " + e.getLocalizedMessage() + ']');
-				return;
-			}
-
-			try {
-				Files.copy(temp.toPath(), f.toPath(), StandardCopyOption.REPLACE_EXISTING);
-			} catch (IOException e) {
-				logger.failure("TMPC", "An encrypted copy of " + f + " was written to a temporary file (" + temp
-						+ ") but an issue occurred when trying to copy that temporary file back over to the source file's location. [Err msg: "
-						+ e.getLocalizedMessage() + ']');
-			}
-			temp.delete();
-
-		} catch (Exception e) {
-			logger.failure("UNKN", "An unknown failure occurred while processing " + f
-					+ ". The file may or may not have been encrypted, but should not be garbage.");
-		}
+		super("encrypt", EncryptionProcessor::encryptFile, key, bufferSize, logger);
 	}
 
 	/**
@@ -135,7 +72,7 @@ public class EncryptionProcessor implements DirectoryProcessor {
 	 *                                            filesystem.
 	 * @throws FileProcessingException            if the file is already encrypted.
 	 */
-	private static void encryptFile(File f, File dest, int bufferSize, byte[] header, byte... key)
+	public static void encryptFile(File f, File dest, int bufferSize, byte[] header, byte... key)
 			throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
 			InvalidAlgorithmParameterException, IOException, FileProcessingException {
 		try (FileInputStream fis = new FileInputStream(f)) {
